f63f58cd23bd0a9db9e32a3af0f4bb54fabddc98
Barak <barakc121@gmail.com>
2025-11-11 11:15:42 +0200

    finished class + main

--- Library.py
+++ Library.py
@@ -1,11 +1,13 @@
+import json
+
 class Library:
 
     def __init__(self):
-        self.list_of_book=[]
+        self.list_of_books=[]
         self.list_of_users=[]
 
     def add_book(self,book):
-        self.list_of_book.append(book)
+        self.list_of_books.append(book)
 
     def add_user(self,user):
         self.list_of_users.append(user)
@@ -17,58 +19,72 @@ class Library:
         for u in self.list_of_users:
             if u.id == user_id:
                 user = u
-                for b in self.list_of_book:
-                    if b.ISBN == book_isbn:
-                        book=b
-                        book.is_available=False
-                        user.borrowed_books.append(book)
-        return f"{user},{book}"
+        
+        for b in self.list_of_books:
+            if b.ISBN == book_isbn:
+                book=b
+                if book.is_available:
+                    book.is_available=False
+                    user.borrowed_books.append(book)
+                else: 
+                    print(f"book {book} is unavailable")
+                    return None
+        user = vars(user)
+        user["borrowed_books"] = [vars(b) for b in user["borrowed_books"]]
+        
+        return user,book
     
 
     def return_book(self,user_id, book_isbn):
         user = None
         book = None
-         
+        
         for u in self.list_of_users:
-            if u.id == user_id:
+            if u == user_id:
                 user = u
-                for b in self.list_of_book:
-                    if b.ISBN == book_isbn:
-                        book=b
-                        book.is_available=True
-                        user.borrowed_books.remove(book)
-        return f"{user},{book}"
+        user = vars(user)
+        user["borrowed_books"] = [vars(b) for b in user["borrowed_books"]]
+        for b in self.list_of_books:
+            if b.ISBN == book_isbn:
+                book=b
+                book.is_available=True
+                user.borrowed_books.remove(book)
+        print(f"{user},{book} returned")
+        book = vars(book)
+        return user,book
 
 
     def list_available_books(self):
-        return self.list_of_book
+        list_available=[]
+        for i in self.list_of_books:
+            if i.is_available == True:
+                 list_available.append(i.ISBN)
+        return list_available
 
-
+    def search_book(self,input):
+        for i in [b for b in self.list_of_books if b.is_available == True]:
+            if i.title==input or i.author==input:
+                return f" the book here"        
+            
+    @staticmethod
+    def read_data_json(data_json):
+        with open(data_json)as data:
+            data_string=json.load(data)
+        return data_string
     
 
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @staticmethod
+    def add_data_of_json(data_json,data):
+        print(data)
+        with open(data_json,"w")as f:
+            json.dump(data, f, indent=4)
 
 
 # if __name__ == '__main__':
-#     lib = Library()
-#     lib.list_of_users.append({"id": 12})
-#     print(lib.borrow_book(152, None))
+#     name_libar=Library()
+
+#     barak=User("barak",11911)
+#     tora=Book("tiire","bkaa",911)
+#     name_libar.add_book(tora)
+#     print(name_libar.borrow_book(111,11))
+#     print(name_libar.list_available_books())
